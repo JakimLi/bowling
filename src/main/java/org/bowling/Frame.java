90b37d384e8f9a18949f9644a640773bdfc0acb4
Jani Peng <1184360257@qq.com>
2017-11-19 18:40:30 +0800

    refactor

--- src/main/java/org/bowling/Frame.java
+++ src/main/java/org/bowling/Frame.java
@@ -1,16 +1,15 @@
 package org.bowling;
 
 class Frame {
-    private boolean bonus;
     private String value;
     private Frame lastFrame;
 
     Frame(String value) {
         this.value = value;
+    }
 
-        if (value.contains("/")) {
-            this.bonus = true;
-        }
+    private boolean bonus() {
+        return this.value.contains("/");
     }
 
     int score() {
@@ -18,19 +17,20 @@ class Frame {
     }
 
     private int lastBonus() {
-        if (lastFrame != null && lastFrame.bonus) {
+        if (lastFrame != null && lastFrame.bonus()) {
             return Integer.valueOf(this.value.substring(0, 1));
         }
-
         return 0;
     }
 
     private int currentScore() {
-        return bonus ? 10 : pins();
+        return bonus() ? 10 : pins();
     }
 
     private int pins() {
-        return value.chars().map(Converter::toNumber).reduce(0, (a, b) -> a + b);
+        return value.chars()
+                .map(Converter::toNumber)
+                .reduce(0, (a, b) -> a + b);
     }
 
     void lastFrame(Frame lastFrame) {
